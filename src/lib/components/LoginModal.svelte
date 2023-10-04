7762f01e899c572473eb42e2d8893820e98e34b9
Madan Kumar <madan@kbxdigital.com>
2023-10-04 08:15:35 +0530

    Login changes

--- src/lib/components/LoginModal.svelte
+++ src/lib/components/LoginModal.svelte
@@ -8,8 +8,46 @@
 	import type { LayoutData } from "../../routes/$types";
 	import Logo from "./icons/Logo.svelte";
 	export let settings: LayoutData["settings"];
+	import Textfield from '@smui/textfield';
 
 	const isIframe = browser && window.self !== window.parent;
+	let valueA = '';
+	let valueB = '';
+	 import axios from 'axios';
+	let responseData = ""; // Store the response data here
+	let isLoading = false;
+	let loginError = false;
+	async function Login() {
+		try {
+		isLoading = true; // Set loading flag while making the API call
+
+		// Replace the URL with your API endpoint
+		const response = await axios.post('https://backend.immigpt.net/login', {
+			// Replace this with the data you want to send in the POST request
+			// For example, if you have a JSON payload, you can define it here
+			email: valueA,
+			password: valueB,
+		});
+		console.log('response.status', response.status)
+		if (response.status === 200) {
+			// If the response status is 200 OK, parse the response data
+			responseData = response.data;
+			window.location.href = '/';
+		} else {
+			// Handle non-200 status codes (e.g., display an error message)
+			responseData = `Error: ${response.statusText}`;
+		}
+		} catch (error) {
+		// Handle any network or other errors;
+		console.log('error.response', error.response.status == 401)
+		if (error.response.status == 401){
+			loginError = true;
+		}
+		responseData = `Error: ${error.message}`;
+		} finally {
+		isLoading = false; // Reset loading flag whether the request succeeds or fails
+		}
+	}
 </script>
 
 <Modal>
@@ -35,10 +73,28 @@
 		{/if}
 		<p class="text-base text-gray-800">
 			<strong>Welcome</strong></p>
-			<label for="username">Username:
-		<input type="text" id="username" name="Email " /> </label>
-		<label for="Password">Password:
-		<input type="text" id="password" pass="password" /></label>
+		<div class="columns margins">
+			<div>
+				<Textfield variant="outlined" bind:value={valueA} id="username" name="Email " label="Username">
+				</Textfield>
+			</div>
+			<div>
+				<Textfield variant="outlined" type="password" bind:value={valueB} id="password" name="Password" label="Password">
+				</Textfield>
+			</div>
+			<button
+				type="submit"
+				class="mt-2 rounded-full bg-black px-5 py-2 text-lg font-semibold text-gray-100 transition-colors hover:bg-primary-500"
+				on:click={Login}
+  				disabled={isLoading} 
+			>
+				{isLoading ? "Loading..." : "Login"}
+			</button>
+		</div>
+		{#if loginError}
+			<p class="error">Failed while logging in</p> <!-- Display the error message in red -->
+		{/if}
+
 		{#if PUBLIC_APP_DATA_SHARING}
 			<p class="px-2 text-sm text-gray-500">
 				Your conversations will be shared with model authors unless you disable it from your
@@ -61,7 +117,7 @@
 						with <LogoHuggingFaceBorderless classNames="text-xl mr-1 ml-1.5" /> Hugging Face
 					{/if}
 				</button>
-			{:else}
+			{:else if $page.data.requiresLogin}
 				<input type="hidden" name="ethicsModalAccepted" value={true} />
 				{#each Object.entries(settings) as [key, val]}
 					<input type="hidden" name={key} value={val} />
@@ -76,3 +132,9 @@
 		</form>
 	</div>
 </Modal>
+
+<style>
+  .error {
+    color: red;
+  }
+</style>
