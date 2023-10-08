ee458e1f0797918c34410d36a4b19665171c86b8
Murugesh Raja <murugesh@kbxdigital.com>
2023-10-09 00:00:03 +0530

    chat screen ui

---
+++ src/lib/components/ChatCard.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { createEventDispatcher } from "svelte";
+	export let cardData;
+</script>
+
+<div class="container">
+	<div class="person">
+		<img src={cardData.imageUrl} alt="" />
+		<span class="name">{cardData.name}</span>
+		<span class="time">{cardData.time}</span>
+		<span class="preview">{cardData.chat}</span>
+	</div>
+</div>
+
+<style>
+	.person {
+		position: relative;
+		width: 100%;
+		padding: 12px 10% 16px;
+		cursor: pointer;
+		background-color: white;
+	}
+
+	img {
+		float: left;
+		width: 40px;
+		height: 40px;
+		margin-right: 12px;
+		border-radius: 50%;
+		object-fit: cover;
+	}
+	.name {
+		font-size: 14px;
+		line-height: 22px;
+		color: black;
+	}
+	.time {
+		font-size: 14px;
+		position: absolute;
+		top: 16px;
+		right: 10%;
+		padding: 0 0 5px 5px;
+		color: grey;
+		background-color: white;
+	}
+	.preview {
+		font-size: 14px;
+		display: inline-block;
+		overflow: hidden !important;
+		width: 70%;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		color: var(--grey);
+	}
+</style>
---
+++ src/routes/chats/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+	import { Input } from "@svelteuidev/core";
+	import { MagnifyingGlass } from "radix-icons-svelte";
+	import ChatCard from "$lib/components/ChatCard.svelte";
+
+	let searchInput = "";
+
+	let friendCards = [
+		{
+			name: "Virat Kohli",
+			time: "2:00 PM",
+			imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/louis-ck.jpeg",
+			chat: "But we’re probably gonna need a new carpet.",
+			id: "1",
+		},
+		{
+			name: "Rohit Sharma",
+			time: "9:00 AM",
+			imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/louis-ck.jpeg",
+			chat: "But we’re probably gonna need a new carpet.",
+			id: "2",
+		},
+		{
+			name: "Virat Kohli",
+			time: "2:00 PM",
+			imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/louis-ck.jpeg",
+			chat: "But we’re probably gonna need a new carpet.",
+			id: "3",
+		},
+
+		{
+			name: "Virat Kohli",
+			time: "2:00 PM",
+			imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/louis-ck.jpeg",
+			chat: "But we’re probably gonna need a new carpet.",
+			id: "4",
+		},
+	];
+</script>
+
+<div class="wrapper">
+	<div class="container">
+		<div class="left-container">
+			<p class="title">Messages</p>
+			<Input
+				icon={MagnifyingGlass}
+				placeholder="Search friends"
+				rightSectionWidth={70}
+				styles={{ rightSection: { pointerEvents: "none" } }}
+				bind:value={searchInput}
+			/>
+			<div class="list-of-friends">
+				{#each friendCards as friendCard}
+					<ChatCard cardData={friendCard} />
+				{/each}
+			</div>
+		</div>
+		<div class="right-container">
+			<span class="chat-screen"> Chat screen </span>
+		</div>
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		padding: 20px;
+	}
+
+	.container {
+		display: flex;
+		gap: 12px;
+	}
+
+	.left-container,
+	.right-container {
+		padding: 20px;
+	}
+
+	.left-container {
+		padding: 20px;
+		gap: 15px;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
