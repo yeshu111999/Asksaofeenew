92ce7b7710f71a50951525ffee9a963c78ceda08
Murugesh Raja <murugesh@kbxdigital.com>
2023-11-13 23:30:25 +0530

    Theme fix in browse templates

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -300,7 +300,7 @@
 		sizes="180x180"
 		type="image/png"
 	/>
-	<meta name="color-scheme" content={$currentTheme == "light" ? "light" : "dark"} />
+	<meta name="color-scheme" content={$currentTheme == "light" ? "light" : "light"} />
 	<link rel="stylesheet" href={`themes/${$currentTheme}.css`} />
 </svelte:head>
 
