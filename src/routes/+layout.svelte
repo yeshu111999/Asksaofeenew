c3b8d09f706bb9eea18a8f394a200bdbc0c3de34
yeshu111999 <119067086+yeshu111999@users.noreply.github.com>
2023-12-30 11:58:04 -0600

    the first open sentence change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -118,7 +118,7 @@
 
 <svelte:head>
 	<title>{PUBLIC_APP_NAME}</title>
-	<meta name="description" content="The first open source alternative to ChatGPT. 💪" />
+	<meta name="description" content="A Generative AI for Immigration and Travel." />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@huggingface" />
 	<meta property="og:title" content={PUBLIC_APP_NAME} />
